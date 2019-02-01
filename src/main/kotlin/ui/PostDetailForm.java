47deed060818704311174425fbb7723e8b570cc7
wlf <wendreoofernandes@gmail.com>
2019-02-01 21:50:16 -0200

    populating the details screen

--- src/main/kotlin/ui/PostDetailForm.java
+++ src/main/kotlin/ui/PostDetailForm.java
@@ -1,5 +1,8 @@
 package ui;
 
+import business.PostBusiness;
+import entity.PostEntity;
+
 import javax.swing.*;
 
 public class PostDetailForm extends JFrame{
@@ -7,12 +10,23 @@ public class PostDetailForm extends JFrame{
     private JLabel lblTitulo;
     private JLabel lblBody;
 
+    private PostBusiness mpoPostBusiness = new PostBusiness();
+
     public PostDetailForm( int id )
     {
         super();
 
+        this.loadPost( id );
+
         this.setContentPane( rootPanel);
         this.setSize( 500, 250 );
         this.setVisible( true );
+    }
+
+    private void loadPost(int id) {
+        PostEntity entity = this.mpoPostBusiness.getSinglePost( id );
+
+        this.lblTitulo.setText(entity.getTitle());
+        this.lblBody.setText("<html>" + entity.getBody() + "</html>");
     }
 }
